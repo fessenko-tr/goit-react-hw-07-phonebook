d729eee95c069b24b6f3755a643831b2b09c0aa8
unknown <fessenko.tr@gmail.com>
2022-01-15 15:42:41 +0200

    добавляет try catch

--- src/redux/phonebook/operations.js
+++ src/redux/phonebook/operations.js
@@ -3,27 +3,37 @@ import MockAPI from "../../api/mockAPI/mockAPI";
 
 export const fetchContacts = createAsyncThunk(
   "contacts/FetchContacts",
-  async () => {
-    const contacts = await MockAPI.get("/contacts");
-
-    return contacts.data;
+  async (thunkAPI) => {
+    try {
+      const contacts = await MockAPI.get("/contacts");
+      return contacts.data;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
 );
 
 export const deleteContactById = createAsyncThunk(
   "contacts/DeleteConctact",
-  async (id) => {
-    const deletedContact = await MockAPI.delete(`/contacts/${id}`);
-
-    return deletedContact.data.id;
+  async (id, thunkAPI) => {
+    try {
+      const deletedContact = await MockAPI.delete(`/contacts/${id}`);
+      return deletedContact.data.id;
+    } catch (error) {
+      return thunkAPI.rejectWithValue(error);
+    }
   }
 );
 
 export const addContact = createAsyncThunk(
   "contacts/AddContact",
-  async (contact) => {
-    const addedContact = await MockAPI.post("/contacts", contact);
-
-    return addedContact.data;
+  async (contact, thunkAPI) => {
+    try {
+      const addedContact = await MockAPI.post("/contacts", contact);
+      return addedContact.data;
+    } catch (error) {
+      console.dir(error);
+      return thunkAPI.rejectWithValue(error);
+    }
   }
 );
